25b66d3d3a503a685422db9b51295779b018bff2
dipdregan <dipendrapratap155@gmail.com>
2024-03-22 14:40:44 +0530

    added dvc in data transformation

--- us_visa/components/data_transformation.py
+++ us_visa/components/data_transformation.py
@@ -202,6 +202,7 @@ class DataTransformation:
                     transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
                     transformed_test_file_path=self.data_transformation_config.transformed_test_file_path
                 )
+                logging.info(data_transformation_artifact)
                 return data_transformation_artifact
             else:
                 raise Exception(self.data_validation_artifact.message)
