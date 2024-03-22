c5a2a048aba1f968a17d711858d6080ab4c19d6d
dipdregan <dipendrapratap155@gmail.com>
2024-03-22 16:10:05 +0530

    model evaluation integrated with model evaluation file

--- test_pkg.py
+++ test_pkg.py
@@ -1,20 +1,35 @@
 from us_visa.components.data_ingestion import DataIngestion
-from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
+from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig,\
+      DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig
 from us_visa.components.data_validation import DataValidation
 from us_visa.components.model_trainer import ModelTrainer
-from us_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
+from us_visa.components.model_evaluation import ModelEvaluation
+from us_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,\
+    DataTransformationArtifact,ModelTrainerArtifact,ClassificationMetricArtifact
 from us_visa.components.data_transformation import DataTransformation
 
-#### ====================== Model trainer testing ==================
-if __name__ =="__main__":
-    config = ModelTrainerConfig()
-    data_trasnformation_artifact = DataTransformationArtifact(transformed_object_file_path='artifact\\03_22_2024_14_41_33\\data_transformation\\transformed_object\\preprocessing.pkl',
-                                                              transformed_train_file_path='artifact\\03_22_2024_14_41_33\\data_transformation\\transformed\\train.npy', 
-                                                              transformed_test_file_path='artifact\\03_22_2024_14_41_33\\data_transformation\\transformed\\test.npy')
 
-    model_trainer = ModelTrainer(data_trasnformation_artifact,
-                                 config)
-    model_trainer.initiate_model_trainer()
+#### ======================== Model Evaluation ====================
+# if __name__ == "__main__":
+#     config = ModelEvaluationConfig()
+#     ingestion =  DataIngestionArtifact(trained_file_path='artifact\\03_22_2024_14_26_23\\data_ingestion\\ingested\\train.csv',
+#                                 test_file_path='artifact\\03_22_2024_14_26_23\\data_ingestion\\ingested\\test.csv')
+#     trainer = ModelTrainerArtifact(trained_model_file_path='artifact\\03_22_2024_15_06_47\\model_trainer\\trained_model\\model.pkl',
+#                                    metric_artifact=ClassificationMetricArtifact(accuracy=0.815070463042853, f1_score=0.8308339910549855, precision_score=0.8562906724511931, recall_score=0.8068472151251916))
+#     obj = ModelEvaluation(config,ingestion,trainer)
+#     obj.initiate_model_evaluation()
+
+
+#### ====================== Model trainer testing ==================
+# if __name__ =="__main__":
+#     config = ModelTrainerConfig()
+#     data_trasnformation_artifact = DataTransformationArtifact(transformed_object_file_path='artifact\\03_22_2024_14_41_33\\data_transformation\\transformed_object\\preprocessing.pkl',
+#                                                               transformed_train_file_path='artifact\\03_22_2024_14_41_33\\data_transformation\\transformed\\train.npy', 
+#                                                               transformed_test_file_path='artifact\\03_22_2024_14_41_33\\data_transformation\\transformed\\test.npy')
+
+#     model_trainer = ModelTrainer(data_trasnformation_artifact,
+#                                  config)
+#     model_trainer.initiate_model_trainer()
 
 ### =========================== Data Transfromation =============================
 # if __name__ == "__main__":
