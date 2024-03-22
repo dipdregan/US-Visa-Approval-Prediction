32c4691e82c00c9ebee4a1940ea19908d69d2e6b
dipdregan <dipendrapratap155@gmail.com>
2024-03-22 15:10:00 +0530

    model training completed

--- test_pkg.py
+++ test_pkg.py
@@ -1,18 +1,29 @@
 from us_visa.components.data_ingestion import DataIngestion
-from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig, DataTransformationConfig
+from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from us_visa.components.data_validation import DataValidation
-from us_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from us_visa.components.model_trainer import ModelTrainer
+from us_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
 from us_visa.components.data_transformation import DataTransformation
 
+#### ====================== Model trainer testing ==================
+if __name__ =="__main__":
+    config = ModelTrainerConfig()
+    data_trasnformation_artifact = DataTransformationArtifact(transformed_object_file_path='artifact\\03_22_2024_14_41_33\\data_transformation\\transformed_object\\preprocessing.pkl',
+                                                              transformed_train_file_path='artifact\\03_22_2024_14_41_33\\data_transformation\\transformed\\train.npy', 
+                                                              transformed_test_file_path='artifact\\03_22_2024_14_41_33\\data_transformation\\transformed\\test.npy')
+
+    model_trainer = ModelTrainer(data_trasnformation_artifact,
+                                 config)
+    model_trainer.initiate_model_trainer()
 
 ### =========================== Data Transfromation =============================
 # if __name__ == "__main__":
 #     config = DataTransformationConfig()
 #     validation_artifact = DataValidationArtifact(validation_status=True, message='Drift not detected',
-#                                     drift_report_file_path='artifact\\03_21_2024_21_50_52\\data_validation\\drift_report\\report.yaml')
+#                                                   drift_report_file_path='artifact\\03_22_2024_14_29_54\\data_validation\\drift_report\\report.yaml')
     
-#     ingestion_artifact =  DataIngestionArtifact(trained_file_path='artifact\\03_21_2024_21_49_02\\data_ingestion\\ingested\\train.csv',
-#                                  test_file_path='artifact\\03_21_2024_21_49_02\\data_ingestion\\ingested\\test.csv')
+#     ingestion_artifact =  DataIngestionArtifact(trained_file_path='artifact\\03_22_2024_14_26_23\\data_ingestion\\ingested\\train.csv',
+#                                                 test_file_path='artifact\\03_22_2024_14_26_23\\data_ingestion\\ingested\\test.csv')
 
 #     data_transformation = DataTransformation(ingestion_artifact,
 #                                              config,
@@ -23,8 +34,8 @@ from us_visa.components.data_transformation import DataTransformation
 
 # if __name__ == "__main__":
 #     config =  DataValidationConfig()
-#     obj =  DataIngestionArtifact(trained_file_path='artifact\\03_21_2024_21_49_02\\data_ingestion\\ingested\\train.csv',
-#                                  test_file_path='artifact\\03_21_2024_21_49_02\\data_ingestion\\ingested\\test.csv')
+#     obj =  DataIngestionArtifact(trained_file_path='artifact\\03_22_2024_14_26_23\\data_ingestion\\ingested\\train.csv',
+#                                                      test_file_path='artifact\\03_22_2024_14_26_23\\data_ingestion\\ingested\\test.csv')
 #     data_validation = DataValidation(data_ingestion_artifact=obj,
 #                    data_validation_config=config)
 
@@ -33,10 +44,10 @@ from us_visa.components.data_transformation import DataTransformation
 
 #### ======================== Data Ingestion ================================
 
-if __name__ == "__main__":
-    config = DataIngestionConfig()
-    data_ingestion = DataIngestion(config)
-    data_ingestion.initiate_data_ingestion()
+# if __name__ == "__main__":
+#     config = DataIngestionConfig()
+#     data_ingestion = DataIngestion(config)
+#     data_ingestion.initiate_data_ingestion()
 
 
 
